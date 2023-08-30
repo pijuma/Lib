393b5c9c86825b90a0d1671ed33467e2d52fdaeb
Pietra <73311085+pijuma@users.noreply.github.com>
2023-08-30 14:32:36 -0300

    Update and rename fft_simple.cpp to fft.cpp

--- C++/Math/fft.cpp
+++ C++/Math/fft.cpp
@@ -1,6 +1,6 @@
-#define ld long double
-const ld PI = acos(-1);
-
+const int maxn = 2e5 + 5 ; 
+const ld PI = acos(-1) ;
+ 
 struct num{
     ld a {0.0}, b {0.0};
     num(){}
@@ -19,7 +19,7 @@ struct num{
          return num(a/c, b/c);
     }
 };
-
+ 
 void fft(vector<num> &a, bool invert){
     int n = a.size();
     for(int i=1,j=0;i<n;i++){
@@ -46,14 +46,14 @@ void fft(vector<num> &a, bool invert){
     if(invert)
       for(num &x: a)
           x = x/n;
-
+ 
 }
-
-vector<ll> multiply(vector<int> const& a, vector<int> const& b){
+ 
+vector<ll> multiply(vi const& a, vi const& b){
     vector<num> fa(a.begin(), a.end());
     vector<num> fb(b.begin(), b.end());
     int n = 1;
-    while(n < int(a.size() + b.size()) )
+    while(n < (a.size() + b.size()) )
         n <<= 1;      
     fa.resize(n);
     fb.resize(n);
@@ -67,4 +67,34 @@ vector<ll> multiply(vector<int> const& a, vector<int> const& b){
         result[i] = round(fa[i].a);
     while(result.back()==0) result.pop_back();
     return result;
+}
+ 
+int main(){
+ 
+    ios_base::sync_with_stdio(false) ; cin.tie(NULL) ;
+ 
+    cin >> k >> n >> m ; 
+ 
+    vector<int> f, f2 ; 
+ 
+    for(int i = 0 ; i <= k ; i++) f.push_back(0), f2.push_back(0) ;
+    
+    for(int i = 1 ; i <= n ; i++){
+        int a ; cin >> a ; 
+        f[a]++ ; 
+    }
+    
+    for(int i = 1 ; i <= m ; i++){
+        int a ; cin >> a ;
+        f2[a]++ ; 
+    }
+ 
+    vector<ll> vv = multiply(f, f2) ;
+ 
+    while(vv.size() < 2*k+1) vv.push_back(0) ;
+ 
+    for(int i = 2 ; i <= 2*k ; i++) cout << vv[i] << " " ;
+    cout << "\n" ;
+ 
+ 
 }
