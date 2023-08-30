80e5a4697d5e959c8c39807188027ba0d89cc019
Bruno Figueiredo Lima <72677862+bruno-lim4@users.noreply.github.com>
2023-08-30 13:50:35 -0300

    Update hash.cpp

--- C++/Strings/hash.cpp
+++ C++/Strings/hash.cpp
@@ -1,27 +1,62 @@
-// String Hash template
-// constructor(s) - O(|s|)
-// query(l, r) - returns the hash of the range [l,r] from left to right - O(1)
-// query_inv(l, r) from right to left - O(1)
+#include<bits/stdc++.h>
+#define int long long 
+using namespace std ; 
 
-const ll MOD = 1e9+7;
-const ll P = 31;
-struct Hash {
-    int n; string s;
-    vector<ll> h, hi, p;
-    Hash() {}
-    Hash(string s): s(s), n(s.size()), h(n), hi(n), p(n) {
-        for (int i=0;i<n;i++) p[i] = (P * (i ? p[i-1]:1)) % MOD;
-        for (int i=0;i<n;i++) {
-            h[i] = ((s[i] - 'a' + 1) * p[i] + (i ? h[i-1]:0)) % MOD;
-            hi[i] = ((s[i] - 'a' + 1) * p[n-i-1] + (i ? hi[i-1]:0)) % MOD;
-        }
+const int maxn = 1e6 + 5 ; 
+const int mod = 1e9 + 7 ; 
+const int prime = 37 ; 
+
+int n, m, t, p[maxn], inv[maxn], pot[maxn], other ; 
+string a, b ; 
+
+int exp(int a, int b){
+
+	int ans = 1 ; 
+
+	if(b == 0) return 1LL ; 
+
+	while(b){
+		if(b&1) ans = (ans*a)%mod ; 
+		a = (a*a)%mod ; 
+		b >>= 1 ; 
+	}
+
+	return ans ; 
+
+}
+
+int32_t main(){
+
+	cin >> a >> b ; 
+
+	n = a.size(), m = b.size() ; 
+
+	pot[0] = 1LL ; 
+
+	for(int i = 1 ; i < maxn ; i++) pot[i] = (pot[i-1]*prime)%mod ; 
+
+	inv[0] = 1LL ; 
+    inv[1] = exp(prime, mod - 2) ; 
+
+    for(int i = 2 ; i < maxn ; i++) inv[i] = (inv[i-1]*inv[1])%mod ; 
+
+    p[0] = ((a[0]-'a')+1) ;
+    
+    for(int i = 1 ; i < n ; i++) p[i] = (p[i-1]+((a[i]-'a'+1)*pot[i])%mod)%mod ;
+
+    other = (b[0]-'a'+1) ; 
+    for(int i = 1 ; i < m ; i++) other = (other + ((b[i]-'a'+1)*pot[i])%mod)%mod ; 
+
+    int ans = 0 ; 
+
+    for(int i = 0 ; i + m - 1 < n ; i++){
+    	int val = p[i+m-1] ; 
+    	if(i) val -= p[i-1] ; 
+    	val = (val + mod)%mod ; 
+    	val = (val*inv[i])%mod ; 
+    	if(val == other) ans++ ; 
     }
-    int query(int l, int r) {
-        ll hash = ((h[r] - (l ? h[l-1]:0)) * p[n-l-1] % MOD);
-        return hash < 0 ? hash + MOD : hash;
-    }
-    int query_inv(int l, int r) {
-        ll hash = ((hi[r] - (l ? hi[l-1]:0)) * p[r] % MOD);
-        return hash < 0 ? hash + MOD : hash;
-    }
-};
+
+    cout << ans << "\n" ; 
+
+}
