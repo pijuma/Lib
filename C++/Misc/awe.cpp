a2719bfcd915da20faa95a605ee5cb4b50e1c2d0
Pietra <73311085+pijuma@users.noreply.github.com>
2024-05-20 17:37:51 -0300

    Create awe.cpp

---
+++ C++/Misc/awe.cpp
@@ -0,0 +1,4 @@
+a.erase() – Remove all instances of element from multiset having the same value
+a.erase(a.find()) – Remove only one instance of element from multiset having same value
+auto it = s.lower_bound({vec[i-1].ss.ff, -1}) ;
+if(it == s.end()) continue ; //se eh igual a s.end() quer dizer que nao esta no set/multiset
