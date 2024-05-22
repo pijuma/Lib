62ba321b527d3f8a356a4f7e9ab868049ecbecc2
Pietra <73311085+pijuma@users.noreply.github.com>
2024-05-21 22:36:53 -0300

    Update ford.cpp

--- C++/Grafos/ford.cpp
+++ C++/Grafos/ford.cpp
@@ -2,23 +2,32 @@
 acha max_flow 
 que é igual em valor ao min_cut 
 com otimização "scaling algorithm" O(m^2*log(MaxC)) - rapido enough 
-Cuidado: quando for grafo denso ou bipartido -> usar Dinic (se constraints forem paias)
 */
+
 #define pb push_back
+#define pii pair<int,int> 
+#define ff first 
+#define ss second 
 
-const int N = 550 ;//qtd vertices
+const int N = 550 ;
 
-int MaxC ; //maior capacidade de aresta dada
+int MaxC ; 
+int ar[N][N] ; //existe aresta de i pra j? 
 
 struct Ford {
     struct Edge {
         int to, f, c;
     };
 
-    int vis[N];
+    int vis[N+2];
     vector<int> adj[N];
     vector<Edge> edges;
-    int cur = 0;
+    int cur, tempo;
+
+    Ford(){
+        cur = 0 ; tempo = 1 ; 
+        for(int i = 0 ; i <= N ; i++) vis[i] = 0 ; 
+    }
 
     void addEdge(int a, int b, int cap, int rcap) {
         Edge e;// cria aresta de ida
@@ -55,7 +64,7 @@ struct Ford {
 
     int flow(int s, int t) {
 
-        int mflow = 0, tempo = 1;
+        int mflow = 0;
         
         for(int i = log2(MaxC) ; i >= 0 ; i--){
            while(int a = dfs(s, t, MaxC, tempo, (1<<i))){
@@ -67,6 +76,31 @@ struct Ford {
         
         return mflow; // fluxo maximo = qtd de camnihos de S pra T 
     
+    }
+
+    void dfs_cut(int v, int p){
+        vis[v] = tempo ; 
+        for(auto a : adj[v]){
+            auto ed = edges[a] ;
+            if(ed.to == p || vis[ed.to] == tempo) continue ;  
+            if(ed.f == ed.c) continue ; 
+            dfs_cut(ed.to, v) ; 
+        }
+    }
+
+    void find_cut(int s, int t, int n){
+       vector<pii> cut ; 
+       flow(s, t) ; //monta o fluxo 
+       tempo++ ; 
+       dfs_cut(s, s) ; // dfs com ini =s passando so pelas arestas n esgotaram = monta uma componente  
+       for(int i = 1 ; i <= n ; i++){ //add a aresta se ela nao for reversa e for de algum visitado pra um nao visitado (dividiu em 2 sets) 
+            for(auto a : adj[i]){
+                auto ed = edges[a] ;  
+                if(vis[i] == tempo && vis[ed.to] != tempo && ar[i][ed.to] == 1){
+                    cut.pb({i, ed.to}) ; 
+                }
+            }
+       }
     }
 
 };
