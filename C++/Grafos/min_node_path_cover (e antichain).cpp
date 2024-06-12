84dbbaba39b2923faf376377de31d876e185139b
Pietra <73311085+pijuma@users.noreply.github.com>
2024-06-12 10:41:21 -0300

    Update min_node_path_cover (e antichain).cpp

--- C++/Grafos/min_node_path_cover (e antichain).cpp
+++ C++/Grafos/min_node_path_cover (e antichain).cpp
@@ -1,5 +1,4 @@
 //cada nó só pertence a um caminho
-// esse código é pra disjoint path cover 
 //pra geral -> cria uma aresta de a->b sempre que 
 // a alcança b, não apenas se a aresta a->b existe
 // a antichain é igual ao min node path cover geral 
