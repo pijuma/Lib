13a5474359f804b608fe5f2368c8cc466fe8be94
Pietra <73311085+pijuma@users.noreply.github.com>
2024-08-28 13:55:53 -0300

    Update dfs_ciclo.cpp

--- C++/Grafos/dfs_ciclo.cpp
+++ C++/Grafos/dfs_ciclo.cpp
@@ -1,20 +1,39 @@
-void dfs(int u, int p){
-	vis[u] = 1 ; 
-	pai[u] = p ; 
-	for(int a : grafo[u]){
-		if(a == p) continue ; 
-		if(vis[a]){
-			vector<int> ans ; 
-			int p = u ; 
-			while(a^u){
-				ans.pb(u) ; 
-				u = pai[u] ;  
-			}
-			ans.pb(a), ans.pb(p) ; 
-			cout << ans.size() << "\n" ; 
-			for(int b : ans) cout << ++b << " " ; 
-			cout << "\n" ; 
-			exit(0) ; 
-		}
-	    else dfs(a, u) ; 
+void dfs(int v, int p){
+	vis[v] = 1 ; 
+	for(auto a : grafo[v]){
+		if(ciclo) continue ; 
+        	//if(a == p) continue ; - em aresta bidirecionada n pode voltar pela mesma 
+		pai[a] = v ; 
+		if(vis[a] == 1){
+			ciclo = v ; pai[a] = 0 ; 
+		} 
+		else if(!vis[a]) dfs(a, v) ; 
 	}
+	vis[v] = 2 ; 
+}
+ 
+void redo(int x){ //recuperar começando em "ciclo" 
+	if(!x) return ; 
+	redo(pai[x]) ; 
+	cam.push_back(x) ; 
+}
+ 
+int main(){
+	cin >> n >> m ; 
+	for(int i = 1 ; i <= m ; i++){
+		int a, b ; cin >> a >> b ; 
+		grafo[a].push_back(b) ; //grafo[b].push_back(a) ;
+	}
+ 
+	for(int i = 1 ; i <= n ; i++){
+		if(!vis[i]) dfs(i, -1) ; 
+	}
+ 
+	if(!ciclo){ cout << "IMPOSSIBLE\n" ; exit(0) ; } 
+	else{
+		cam.push_back(ciclo) ; redo(ciclo) ; 
+		cout << cam.size() << "\n" ;  
+		for(auto a : cam) cout << a << " " ; 
+	}
+ 
+}
