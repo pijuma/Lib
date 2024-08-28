72e9f25126fa064c2ca92ad6d7bc5b53c8a8c663
Pietra <73311085+pijuma@users.noreply.github.com>
2024-08-28 10:48:54 -0300

    Update bellman_ford.cpp

--- C++/Grafos/bellman_ford.cpp
+++ C++/Grafos/bellman_ford.cpp
@@ -1,3 +1,21 @@
+//maior soma de valores de 1 a n: 
+void bell(int s){
+    
+    for(int i = 1 ; i <= n ; i++) d[i] = -inf ; d[s] = 0 ;  
+    for(int i = 1 ; i <= n ; i++){
+    	for(int j = 1 ; j <= m ; j++){
+            if(d[u[j]] == -inf) continue ; 
+    		if(d[u[j]] + w[j] > d[v[j]]) d[v[j]] = d[u[j]] + w[j] ;
+    	}
+    }
+
+    for(int i = 1 ; i <= n ; i++){
+    	for(int j = 1 ; j <= m ; j++){
+    		if(d[u[j]] != -inf && d[u[j]] + w[j] > d[v[j]]) d[v[j]] = inf ;
+    	}
+    }
+}
+//achar ciclo negativo: 
 bool bel(int s){
 
 	for(int i = 0 ; i <= n ; i++) d[i] = inf ; 
