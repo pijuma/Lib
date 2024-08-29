c69711a39b3097e39d5a32bbde049120e3ca08d1
Pietra <73311085+pijuma@users.noreply.github.com>
2024-08-29 15:30:48 -0300

    Update seg_pa.cpp

--- C++/ED/SegTree/seg_pa.cpp
+++ C++/ED/SegTree/seg_pa.cpp
@@ -1,13 +1,12 @@
-int tree[4*maxn] ;
-int type, n, q, l, r, a, d, pos, m ; 
-pair<int, int> lazy[4*maxn] ; 
+int n, q, v[maxn], tree[4*maxn] ; 
+pair<int,int> lazy[4*maxn] ;
  
 struct SEG{
- 
+	
 	void build(int no, int i, int j){
-		lazy[no] = make_pair(0, 0) ; 
+		lazy[no] = {0, 0} ; 
 		if(i == j){
-			tree[no] = 0 ; 
+			tree[no] = v[i] ; 
 			return ; 
 		}
 		build(esq, i, meio), build(dir, meio + 1, j) ; 
@@ -15,35 +14,34 @@ struct SEG{
 	}
  
 	void flush(int no, int i, int j){
-		if(lazy[no] == make_pair(0LL, 0LL)) return ; 
-		tree[no] += lazy[no].first + lazy[no].second*i ; 
+		if(!lazy[no].first) return ; 
+		int n = (j-i+1), a1 = lazy[no].second, q = lazy[no].first ; 
+		int an = (a1 + (n-1)*q) ; 
+		tree[no] += ((a1+an)*n)/2 ; 
 		if(i != j){
-			lazy[esq] = make_pair(lazy[esq].first + lazy[no].first, lazy[esq].second + lazy[no].second) ; 
-			lazy[dir] = make_pair(lazy[dir].first + lazy[no].first, lazy[dir].second + lazy[no].second) ;
+			lazy[esq].first += q, lazy[dir].first += q ; 
+			lazy[esq].second += a1, lazy[dir].second += ((a1 + (meio + 1 - i)*q)) ;  
 		}
-		lazy[no] = make_pair(0, 0) ; 
+		lazy[no] = {0, 0} ; 
 	}
  
-	//só o 1o cara importa
- 
-	void att(int no, int i, int j, int l, int r, int a, int d){
-		flush(no, i, j) ; 
-		if(l > j || r < i) return ; 
-		if(l <= i && r >= j){
-			lazy[no] = make_pair(a - d*l, d) ; 
+	void upd(int no, int i, int j, int l, int r){
+		flush(no, i, j) ;
+		if (i > r || j < l) return ; 
+		if (i >= l && j <= r){
+			lazy[no] = {1LL, i - l + 1} ; // razao e val inicial 
 			flush(no, i, j) ; 
 			return ; 
 		}
-		att(esq, i, meio, l, r, a, d), att(dir, meio + 1, j, l, r, a, d) ; 
+		upd(esq, i, meio, l, r), upd(dir, meio + 1, j, l, r) ; 
 		tree[no] = tree[esq] + tree[dir] ; 
 	}
  
-	int query(int no, int i, int j, int pos){
-		flush(no, i, j) ;
-		if(i == j && i == pos) return tree[no] ; 
-		if(pos <= meio) return query(esq, i, meio, pos) ; 
-		else return query(dir, meio + 1, j, pos) ; 
+	int query(int no, int i, int j, int l, int r){
+		flush(no, i, j) ; 
+		if (i > r || j < l) return 0LL ; 
+		if (i >= l && j <= r) return tree[no] ;
+		return query(esq, i, meio, l, r) + query(dir, meio + 1, j, l, r) ;  
 	}
  
-} seg ;
- 
+} Seg ;
