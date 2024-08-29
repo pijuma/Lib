63a11217b55ad8f790457f6d45d0a48779af4baa
Arjuna <137313409+TransfiniteSoul@users.noreply.github.com>
2024-08-29 18:25:29 -0300

    Create mobius.cpp

---
+++ C++/Math/mobius.cpp
@@ -0,0 +1,24 @@
+bool is_composite[MAX];
+int mu[MAX];
+
+void siege(){
+    mu[1] = -1;
+    for(int i=2; i<MAX; i++){
+        if(is_composite[i]) continue;
+        mu[i] = -1;
+        for(int j=i*i; j<MAX; j+=i)
+            is_composite[j] = true;
+    }
+}
+
+void mobius(){
+    siege();
+    
+    for(int i=4; i<MAX; i++){
+        if(mu[i]!=0) continue;
+        for(int j=2; j*j<=i; j++)
+            if(i%j==0)
+                if((i/j)%j!=0)
+                    mu[i] = mu[j]*mu[i/j];
+    }
+}
