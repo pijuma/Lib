e678be5f1fc3fbd511064251d01370e917fcd510
Pietra <73311085+pijuma@users.noreply.github.com>
2025-04-19 19:58:38 -0300

    Update kmp.cpp

--- C++/Strings/kmp.cpp
+++ C++/Strings/kmp.cpp
@@ -1,35 +1,63 @@
-//pi[i] = maior pref [0, ...i] tal que é um sufixo de [i, i-1,...,i-k+1] 
-vector<int> table(){
-	vector<int> ans(pattern.size(), 0) ; 
-	int j = 0 ; 
-	for(int i = 1 ; i < pattern.size() ; i++){
-		while(j > 0 && pattern[i] != pattern[j]) j = ans[j-1] ; 
-		if(pattern[i] == pattern[j]) j++ ; 
-		ans[i] = j ; 
-	}
-	return ans ; 
-}
+struct KMP{
+    
+    vector<int> phi ;
 
-vector<int> kmp(vector<int> pi){
-	vector<int> ans(s.size(), 0) ; 
-	int j = 0 ; 
-	for(int i = 0 ; i < s.size() ; i++){
-		while(j > 0 && pattern[j] != s[i]) j = pi[j-1] ; 
-		if(pattern[j] == s[i]) j++ ; 
-		ans[i] = j ; 
-	}
-	return ans ; 
-}
+    vector<int> prefix_kmp(string s){
+        
+        phi.resize(s.size()+1, 0) ;
 
-string p;
-int neighbor[N];
-int walk(int u, char c) { // leader after inputting `c`
-    while (u != -1 && (u+1 >= (int)p.size() || p[u + 1] != c)) // leader doesn't match
-        u = neighbor[u];
-    return p[u + 1] == c ? u+1 : u;
-}
-void build() {
-    neighbor[0] = -1; // -1 is the leftmost state
-    for (int i = 1; i < (int)p.size(); i++)
-        neighbor[i] = walk(neighbor[i-1], p[i]);
+        for(int i = 1 ; i < s.size() ; i++){
+            
+            int &j = phi[i] ; 
+            j = phi[i-1] ; 
+
+            //vou pulando pelos sufixos (pra pegar os prefs)
+            while(j > 0 && s[i] != s[j]) j = phi[j-1] ; 
+
+            if(s[i] == s[j]) j++ ; 
+
+        }
+
+        return phi ; 
+
+    }
+
+} ;
+
+/*
+quero pegar todos os sufixos que batem com prefixos -> vou andando no sufixo do prefixo pelo phi
+preciso saber quantas vezes cada um apareceu:
+freq[res[i-1]] += (freq[i]+1) um prefixo/sufixo menor ta incluso num maior que vem antes dele 
+*/
+
+int main(){
+
+    string s ; cin >> s ; 
+    
+    KMP A ; 
+
+    vector<int> res = A.prefix_kmp(s) ; 
+
+    vector<int> pos ; 
+
+    for(int i = s.size()-1 ; i >= 0 ; i = res[i] - 1){//pega o maior suf do pref q peguei
+        pos.pb(i+1) ; 
+    } 
+
+    cout  << pos.size() << "\n" ;
+
+    sort(pos.begin(), pos.end()) ; 
+
+    vector<int> freq(s.size()+2, 0) ; 
+
+    freq[s.size()] = 1 ; 
+
+    for(int i = s.size() ; i >= 1 ; i--){
+        freq[res[i-1]] += (freq[i]+1)  ;
+    }
+    
+    for(auto a : pos){
+        cout << a << " " << freq[a] << "\n" ; 
+    }
+
 }
