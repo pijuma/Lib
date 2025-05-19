60d0f79454b516157dcdf37617ffec92700a59ca
Pietra <73311085+pijuma@users.noreply.github.com>
2025-05-19 19:18:56 -0300

    Update suffix_array(cp).cpp

--- C++/Strings/suffix_array(cp).cpp
+++ C++/Strings/suffix_array(cp).cpp
@@ -61,3 +61,26 @@ int compare(int i, int j, int l, int k) {
     pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
     return a == b ? 0 : a < b ? -1 : 1;
 }
+
+vector<int> lcp_construction(string const& s, vector<int> const& p) {
+    int n = s.size();
+    vector<int> rank(n, 0);
+    for (int i = 0; i < n; i++)
+        rank[p[i]] = i;
+
+    int k = 0;
+    vector<int> lcp(n-1, 0);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        int j = p[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i+k] == s[j+k])
+            k++;
+        lcp[rank[i]] = k;
+        if (k)
+            k--;
+    }
+    return lcp;
+}
