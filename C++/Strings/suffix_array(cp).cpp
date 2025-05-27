3b77ec66fc2099db90d4b68a58f5f2f4ecd447cf
Pietra <73311085+pijuma@users.noreply.github.com>
2025-05-27 15:21:19 -0300

    Update suffix_array(cp).cpp

--- C++/Strings/suffix_array(cp).cpp
+++ C++/Strings/suffix_array(cp).cpp
@@ -1,86 +1,198 @@
 /*
+esse código é a implementação para achar a maior substring que existe em s e t 
+O(nlogn)
 Menor shift ciclico -> comeca em p[0]
-checar se tem substring s em um texto t -> faz SA em t e busca binaria (caderno rosa) 
-checar se duas substrings são iguais -> função compare + sparse table 
+checar se duas substrings são iguais -> função compare + sparse table
 */
+void radix(vector<pair<pair<int, int>, int>> &vec){
 
-vector<int> sort_cyclic_shifts(string const& s) {
-    int n = s.size();
-    const int alphabet = 256;
-    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
-    for (int i = 0; i < n; i++)
-        cnt[s[i]]++;
-    for (int i = 1; i < alphabet; i++)
-        cnt[i] += cnt[i-1];
-    for (int i = 0; i < n; i++)
-        p[--cnt[s[i]]] = i;
-    c[p[0]] = 0;
-    int classes = 1;
-    for (int i = 1; i < n; i++) {
-        if (s[p[i]] != s[p[i-1]])
-            classes++;
-        c[p[i]] = classes - 1;
+    //ordena pelo segundo, conta quantos valores de cada
+    //separa um vetor pra colocar todo mundo ordenado 
+
+    int n = vec.size() ; 
+    vector<int> cnt(n+1, 0) ; 
+
+    for(auto a : vec) cnt[a.ff.ss]++ ; 
+
+    vector<pair<pii,int>> ord_2(n) ; 
+    vector<int> pos(n+1) ; //pra cada bucket qual 1a pos livre? 
+
+    pos[0] = 0 ; //bucket 0 ta livre em 0 
+
+    for(int i = 1 ; i < n ; i++){
+        pos[i] = pos[i-1] + cnt[i-1] ; 
     }
-    vector<int> pn(n), cn(n);
-    for (int h = 0; (1 << h) < n; ++h) {
-        for (int i = 0; i < n; i++) {
-            pn[i] = p[i] - (1 << h);
-            if (pn[i] < 0)
-                pn[i] += n;
-        }
-        fill(cnt.begin(), cnt.begin() + classes, 0);
-        for (int i = 0; i < n; i++)
-            cnt[c[pn[i]]]++;
-        for (int i = 1; i < classes; i++)
-            cnt[i] += cnt[i-1];
-        for (int i = n-1; i >= 0; i--)
-            p[--cnt[c[pn[i]]]] = pn[i];
-        cn[p[0]] = 0;
-        classes = 1;
-        for (int i = 1; i < n; i++) {
-            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
-            if (cur != prev)
-                ++classes;
-            cn[p[i]] = classes - 1;
-        }
-        c.swap(cn);
+
+    for(auto a : vec){//colocar no bucket certo 
+        int bucket = a.ff.ss ; 
+        ord_2[pos[bucket]] = a ; 
+        pos[bucket]++ ; 
     }
-    return p;
+
+    vec = ord_2 ; //vetor ordenado pelo 2o valor
+    
+    //usa o valor ordenado pelo 2o e ordena pro 1o 
+    //sem perder a ordem que temos 
+
+    for(int i = 0 ; i < n ; i++) cnt[i] = 0 ; 
+
+    for(auto a : vec) cnt[a.ff.ff]++ ; 
+    pos[0] = 0 ; 
+
+    for(int i = 1 ; i < n ; i++) pos[i] = pos[i-1] + cnt[i-1] ; 
+
+    for(auto a : vec){
+        int buck = a.ff.ff ; 
+        ord_2[pos[buck]] = a ; 
+        pos[buck]++ ; 
+    }
+
+    vec = ord_2 ; 
+
 }
 
+vector<int> sort_cyclic_shifts(string const &s) {
+    int n = s.size();
+   
+    vector<int> p(n), c(n) ; // p mantem a ordem e c a classe do kra i 
+    vector<pair<char,int>> a(n) ;
+    for(int i = 0 ; i < n ; i++) a[i] = {s[i], i} ; 
+    sort(a.begin(), a.end()) ;
+    for(int i = 0 ; i < n ; i++) p[i] = a[i].second ; //q id ta é o ith menor
+    
+    c[p[0]] = 0 ; //1a classe 
+    for(int i = 1 ; i < n ; i++){//como ta ordenado se estiverem juntos tem a mesma classe 
+        c[p[i]] = c[p[i-1]];
+        if(a[i].first != a[i-1].first) c[p[i]]++ ;
+    }
+    //classes e ordem incialmente definidas 
+    // se fizer em nlog2 -> simplesmente cria o vector e ordena 
+   for(int k = 0 ; (1<<k) < n ; k++){
+       vector<pair<pair<int, int>, int>> strs ; 
+       for(int i = 0 ; i < n ; i++) strs.push_back({{c[i], c[(i+(1<<k))%n]}, i}) ; 
+       // qual classe da 1a metade e da 2a metade 
+       // se a classe eh menor ent ela veio antes na ordenação anterior 
+       radix(strs) ; // com isso nlog 
+       //recriar o p e o c 
+       for(int i = 0 ; i < n ; i++) p[i] = strs[i].second ; 
+       c[p[0]] = 0 ; 
+       for(int i = 1 ; i < n ; i++){
+           c[p[i]] = c[p[i-1]] ;
+           if(strs[i].first != strs[i-1].first) c[p[i]]++ ; 
+       }
+   } 
+   
+   return p ; 
+    
+}
+
+//se não quiser considerar o sufixo vazio, remover o 1o elemento
 vector<int> suffix_array_construction(string s) {
-    s += "$";
-    vector<int> sorted_shifts = sort_cyclic_shifts(s);
+    s += "#";
+    vector<int> sorted_shifts = sort_cyclic_shifts(s); 
     sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
 }
 
-int compare(int i, int j, int l, int k) {
-    pair<int, int> a = {c[k][i], c[k][(i+l-(1 << k))%n]};
-    pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
-    return a == b ? 0 : a < b ? -1 : 1;
+
+string s ; 
+
+int check(int pos, string p){
+    int n = s.size() ; 
+    for(int i = 0 ; i < p.size() ; i++){
+        if(i + pos >= n) return 2 ; //terminou o sufixo
+        char a = s[(i+pos)], b = p[i]; 
+        if(a == '$') return 2 ; 
+        if(a == b) continue ; 
+        if(a < b) return 2 ;
+        return -1 ; 
+    }
+    return 1 ; 
 }
 
-vector<int> lcp_construction(string const& s, vector<int> const& p) {
-    int n = s.size();
-    vector<int> rank(n, 0);
-    for (int i = 0; i < n; i++)
-        rank[p[i]] = i;
+void sub(vector<int> &sa, string p){//p é substring em sa? 
+    
+    int ini = 0, fim = sa.size() - 1, mid, best = -1 ; 
 
-    int k = 0;
-    vector<int> lcp(n-1, 0);
-    for (int i = 0; i < n; i++) {
-        if (rank[i] == n - 1) {
-            k = 0;
-            continue;
+    while(ini <= fim){        
+        mid = (ini + fim)>>1 ;  
+        int c = check(sa[mid], p) ; 
+
+        if(c == 1){ // é sub 
+            best = mid ;  fim = mid - 1 ; 
         }
-        int j = p[rank[i] + 1];
-        while (i + k < n && j + k < n && s[i+k] == s[j+k])
-            k++;
-        lcp[rank[i]] = k;
-        if (k)
-            k--;
+        else if(c < 0){ //p menor que sa 
+            fim = mid - 1 ; 
+        }
+        else ini = mid + 1 ; 
+
     }
-    return lcp;
+
+    if(best == -1) {cout << "0\n" ; return ; }
+
+    ini = best, fim = sa.size()-1 ; int best2 = best ; 
+
+    while(ini <= fim){
+        mid = (ini + fim)>>1 ; 
+        int c = check(sa[mid], p) ; 
+        if(c == 1){
+            best2 = mid, ini = mid + 1 ; 
+        }
+        else if(c < 0) fim = mid - 1 ; 
+        else ini = mid + 1 ; 
+    }
+    cout << best2-best+1 << "\n" ; 
+
+}
+
+vector<int> compute_lcp(vector<int> &sa){
+
+    int n = s.size() ; int k = 0 ; 
+    vector<int> lcp(n, 0) ;
+    vector<int> where(n, 0) ; 
+
+    for(int i = 0 ; i < n ; i++) where[sa[i]] = i ; 
+
+    for(int i = 0 ; i < n ; i++){
+        int psa = where[i] ; //onde eu to no sa 
+        if(psa == 0){
+            k = 0 ; continue ; 
+        }
+        int pant = sa[psa-1] ; //qual pos ta antes de mim  
+        while(i + k < n && pant + k < n && s[i+k] == s[pant+k]){
+            k++ ;  
+        }
+        lcp[psa] = k ; //lcp da posição ordenada  
+        if(k) k-- ; //pode reduzir uma unidade pro prox 
+
+    }
+    return lcp ; 
+}
+
+int32_t main(){
+
+    string s1, s2 ; cin >> s1 >> s2 ; 
+
+    s = s1 + '$' + s2 ; 
+
+    vector<int> ans = suffix_array_construction(s) ; 
+
+    vector<int> lcp = compute_lcp(ans) ; 
+
+    int corte = s1.size() ; 
+    int mx = 0 ; int id = -1 ; 
+
+    for(int i = 1 ; i < ans.size() ; i++){
+        int j = ans[i], k = ans[i-1] ; 
+        if(j >= corte && k >= corte) continue ; 
+        if(j < corte && k < corte) continue ; 
+        if(lcp[i] > mx){
+            mx = lcp[i] ; 
+            id = j ;
+        }
+    }
+
+    for(int i = id ; i < id + mx ; i++) cout << s[i] ; 
+    cout << "\n" ; 
+
 }
