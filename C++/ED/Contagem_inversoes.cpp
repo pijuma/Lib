5a90a42a8200165f2e9d27693d8d0f7079b780ac
Pietra <73311085+pijuma@users.noreply.github.com>
2026-09-02 17:59:51 -0300

    Refactor input handling in Contagem_inversoes.cpp

--- C++/ED/Contagem_inversoes.cpp
+++ C++/ED/Contagem_inversoes.cpp
@@ -1,10 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std ; 
-
-const int maxn = 6e4 + 5 ; 
-
-int n, bit[maxn], ans ; 
-
 struct BIT{
 
 	void upd(int pos, int v){
@@ -21,13 +14,9 @@ struct BIT{
 
 int main(){
 
-	cin >> n ; 
-
 	for(int i = 1 ; i <= n ; i++){
-		int x ; 
-		cin >> x ; 
-		ans += Bit.query(x+1) ;
-		Bit.upd(x, 1) ;  
+		int x ; cin >> x ; 
+		ans += Bit.query(x+1) ; Bit.upd(x, 1) ;  
 	}
 
 	cout << ans << "\n" ; 
